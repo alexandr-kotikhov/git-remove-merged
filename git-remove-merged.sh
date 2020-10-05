b0e9b6190101ef84143cec5c95cc343bb0507312
Alexandr Kotikhov <46875837+alexandr-kotikhov@users.noreply.github.com>
2020-10-05 10:49:06 +0300

    Translation to English and small improvements

--- git-remove-merged.sh
+++ git-remove-merged.sh
@@ -10,18 +10,19 @@ declare DRY_RUN='YES'
 declare INCLUDE=''
 declare EXCLUDE=''
 declare SINCE='7 days ago'
+declare MASTER='master' # skipped
 
 function showHelp() {
     echo ""
-    echo "Удаление смерженных веток"
+    echo "Deletes merged branches."
     echo ""
-    echo "Использование:"
-    echo "git-remove-merged.sh [-r] [-b base-branch] [-i bash-reg-exp] [-e bash-reg-exp] [-s 'duration expression']"
+    echo "USAGE:"
+    echo "git-remove-merged.sh [-r] [-b base-branch] [-i bash-reg-exp] [-e bash-reg-exp] [-s 'duration expression'] [-m master-branch-name]"
     echo ""
-    echo "пример: git-remove-merged.sh -r -b master -i 'XC-.*' | tee git-remove-merged.log"
+    echo "Example: git-remove-merged.sh -r -b master -i 'XC-.*' | tee git-remove-merged.log"
 }
 
-while getopts "i:e:b:d:s:rh" opt; do
+while getopts "i:e:b:d:m:s:rh" opt; do
   case ${opt} in
     r ) DRY_RUN='NO';;
 
@@ -33,36 +34,38 @@ while getopts "i:e:b:d:s:rh" opt; do
 
     s ) SINCE="$OPTARG";;
 
+    m ) MASTER="$OPTARG";;
+
+    h ) 
+      showHelp >&2
+      exit 0
+      ;;
+
     \? )
-      echo "Недопустимая опция: -${opt}." 1>&2
       showHelp >&2
       exit 1
       ;;
-    : )
-      echo "Не задан аргумент у опции -${opt}." 1>&2
-      showHelp >&2
-      exit 1
-      ;;
+      
   esac
 done
 
-echo "Удаление веток, которые уже вмержены базовую ветку '${BASE}' более ${SINCE},"
-echo "  включая ветки по маске = '$INCLUDE',"
-echo "  исключая ветки по маске '$EXCLUDE',"
+echo "Removes branches that were already merged into the branch '${BASE}' more than ${SINCE},"
+echo "  including branches by mask '$INCLUDE',"
+echo "  excluding branches by mask '$EXCLUDE',"
 if [[ "$DRY_RUN" == "YES" ]]; then
-    echo "  xолостой прогон активирован, для включения в режиме обновления укажите параметр '-r'."
+    echo "  dry run is active, to remove merged branches set '-r' option."
 fi
 
-echo "Синхронизация с сервером..."
+echo "Pull the server..."
 git pull
 
-echo "Получение списка смерженных веток..."
+echo "Getting merged branches..."
 BRANCHES=( $(git branch -a --merged "$BASE" --format='%(refname)') )
 
-echo "Все смерженные ветки, ${#BRANCHES[@]}:"
+echo "Merged branches:"
 echo "${BRANCHES[@]}"
 
-echo "Обработка..."
+echo "Total ${#BRANCHES[@]} merged branches. Processing..."
 
 NOT_DELETED=()
 for refname in "${BRANCHES[@]}"; do
@@ -78,55 +81,53 @@ for refname in "${BRANCHES[@]}"; do
   fi
 
   if [[ "$branch" == "$BASE" ]]; then
-    NOT_DELETED+=("$branch - базовая")
-    echo "  пропускаем, так-как это базовая ветка."
+    NOT_DELETED+=("$branch - base")
+    echo "  skipped, because it is the base branch."
     continue
-  elif [[ "$branch" == "master" ]]; then
+  elif [[ "$branch" == "$MASTER" ]]; then
     NOT_DELETED+=("$branch - master")
-    echo "  пропускаем, так-как это master ветка."
+    echo "  skipped, because it is the master branch."
     continue
   elif [[ ! "$INCLUDE" == "" && ! "$branch" =~ $INCLUDE ]]; then
-    NOT_DELETED+=("$branch - не подходит")
-    echo "  пропускаем, так-как не попадает под условие $INCLUDE."
+    NOT_DELETED+=("$branch - not match")
+    echo "  skipped, not match."
     continue
   elif [[ ! "$EXCLUDE" == "" && "$branch" =~ $EXCLUDE ]]; then
-    NOT_DELETED+=("$branch - исключена")
-    echo "  пропускаем, так-как попадает под исключение $EXCLUDE."
+    NOT_DELETED+=("$branch - excluded")
+    echo "  skipped, excluded."
     continue
   elif [[ "$branch" == "HEAD" ]]; then
-    NOT_DELETED+=("$branch - текущая")
-    echo "  пропускаем, так-как это текущая ветка."
+    NOT_DELETED+=("$branch - current")
+    echo "  skipped, because it is the current branch."
     continue
   fi
 
   commit_hash=$(git merge-base "$refname" "$BASE")
   common_commit=$(git log -1 "$commit_hash" --since="$SINCE")
 
-  echo "Последний коммит в ветке:"
+  echo "The last commit in the branch:"
   git log -1 ${commit_hash} | head -n 10
   commit_description=$(git log -1 ${commit_hash} --format='%aI, %aN, %s')
 
   if [[ ! "$common_commit" == "" ]]; then
     NOT_DELETED+=("$branch - не старая: $commit_description")
-    echo "Пропускаем, так-как эта ветка не достаточно старая."
+    echo "  skipped, this branch isn't that old."
   else
 
+    echo "Removing the branch '$refname'..."
     if [[ "$DRY_RUN" == 'NO' ]]; then
       if [[ "$origin" == "" ]]; then
-        echo "Удаление ветки $refname> git branch -d $branch --no-verify"
         git branch -d "$branch" --no-verify
-        echo "Успешно удалена локальная ветка."
+        echo "The local branch is successfuly removed."
       else
-        echo "Удаление ветки $refname> git push $origin --delete $branch --no-verify"
         git push "$origin" --delete "$branch" --no-verify
-        echo "Успешно удалена ветка с сервера."
+        echo "The branch is successfuly removed from the server."
       fi
     else
-      echo "Эмуляция удаления ветки $refname:"
       if [[ "$origin" == "" ]]; then
-        echo "git branch -d $branch --no-verify"
+        echo "emulation> git branch -d $branch --no-verify"
       else
-        echo "git push $origin --delete $branch --no-verify"
+        echo "emulation> git push $origin --delete $branch --no-verify"
       fi
     fi
 
@@ -135,7 +136,7 @@ for refname in "${BRANCHES[@]}"; do
 done
 
 
-echo "Смерженные в ${BASE} ветки, но оставшиеся не удаленными:"
+echo "Merged into '${BASE}' branches but still not deleted:"
 for branch in "${NOT_DELETED[@]}"; do
   echo "  ${branch}"
 done
